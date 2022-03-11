4283a95f8a830b25983c4edaba416fae4d164063
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-03-11 14:53:34 +0200

    added primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SplitButtonModule} from "primeng/splitbutton";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        SplitButtonModule,
+        ButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
