ea412df1669b51138e8287490d7e742ef08aa9c8
Adrian Berejnec <adrianberejnec29@gmail.com>
2022-03-11 15:37:25 +0200

    created data file

---
+++ src/app/data.ts
@@ -0,0 +1,20 @@
+const classes = [
+  {
+    name: "Graphics",
+    teacher: "Kyle",
+    maxNoOfStudents: 10,
+    students: [{name: 'John'}, {name: 'Beth'}, {name: 'Mike'}]
+  },
+  {
+    name: "Mechatronics",
+    teacher: "Allen",
+    maxNoOfStudents: 15,
+    students: [{name: "Stewie"}, {name: "Chris"}, {name: "Peter"}, {name: "Brian"}]
+  },
+  {
+    name: "Robotics",
+    teacher: "Anna",
+    maxNoOfStudents: 5,
+    students: [{name: "Andrew"}, {name: "Leia"}]
+  }
+]
